3861075ed24d9182551ed5198a26a9c4e5e41d00
vishal rajput <106919588+vishal0316@users.noreply.github.com>
2023-02-21 13:27:48 +0530

    Add files via upload

---
+++ paramid.js
@@ -0,0 +1,12 @@
+//How to display pyramid using JavaScript?
+function generatePyramid() {
+    var totalNumberofRows = 5;
+    var arr = new Array();
+    for (var i = 1; i <= totalNumberofRows; i++) {
+      for (var j = 1; j <= i; j++) {
+        arr.push(j);
+        console.log(j);
+      }
+      console.log("\n");
+    }
+  }
